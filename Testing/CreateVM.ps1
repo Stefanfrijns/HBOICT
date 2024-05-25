5c63fada654cf5081dd9d90474890b9bc8419356
Stefanfrijns <153369666+Stefanfrijns@users.noreply.github.com>
2024-05-25 18:09:45 +0200

    Update CreateVM.ps1

--- Testing/CreateVM.ps1
+++ Testing/CreateVM.ps1
@@ -126,7 +126,7 @@ try {
 
     # Attach the VDI
     Log-Message "Attaching VDI..."
-    & "$vboxManagePath" storageattach $VMName --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$($vdiFilePath.FullName)"
+    & "$vboxManagePath" storageattach $VMName --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $vdiFilePath.FullName
     Log-Message "VDI attached successfully."
 
     # Configure boot order
